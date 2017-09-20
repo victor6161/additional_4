4ae2145168deebc5e80c7a0fc4d1bdbdafd761e5
Viktar Kazlou <KazlouV@iba>
2017-09-20 12:45:46 +0300

    однозначное на многозначное

--- src/index.js
+++ src/index.js
@@ -1,3 +1,30 @@
 module.exports = function multiply(first, second) {
-  // your solution
+  
+  var digit;
+  var additive = 0;
+  var currentNumber;
+  var itemString = "";
+  
+  for( var i = 0; i<second.length; i++){
+    
+    for(var j = 0; j < first.length; j++){
+      digit = first[first.length - j - 1];
+      digit = digit * second[i];
+      currentNumber = parseInt(digit%10) + additive;
+    
+      if(currentNumber/10 >= 1){
+        additive = parseInt(digit/10) + 1;
+        currentNumber = parseInt(currentNumber%10);
+      }else{
+        additive = parseInt(digit/10);
+      }
+      itemString = itemString + currentNumber;
+    }
+    itemString += additive; 
+    itemString = itemString.split("").reverse().join("");
+    
+  }
+
+  return itemString;
 }
+
