00e2a17e271db02119ad1bcffa5254356d1a8234
Viktar Kazlou <KazlouV@iba>
2017-09-27 15:29:56 +0300

    -

--- src/index.js
+++ src/index.js
@@ -5,9 +5,11 @@ module.exports = function multiply(first, second) {
   var currentNumber;
   var itemString = "";
   
-  for( var i = 0; i<second.length; i++){
-    
-    for(var j = 0; j < first.length; j++){
+  var itemsString = [];
+  
+
+    for(let i = 0; i < second.length; i++){
+    for(let j = 0; j < first.length; j++){
       digit = first[first.length - j - 1];
       digit = digit * second[i];
       currentNumber = parseInt(digit%10) + additive;
@@ -20,11 +22,70 @@ module.exports = function multiply(first, second) {
       }
       itemString = itemString + currentNumber;
     }
-    itemString += additive; 
-    itemString = itemString.split("").reverse().join("");
+      itemString += additive; 
+      itemString = itemString.split("").reverse().join("");
+      itemsString.push(itemString);
+      
+      itemString='';
+    }
+    let k = 0;
+    itemsString = itemsString.reverse(); 
+    for(let i = 0; i < itemsString.length ; i++ ){
+      
+      for( let ii = 0; ii< k; ii++){//цикл для добавления нулей в начало
+        itemsString[i] = itemsString[i] + "0"; 
+      }
+      itemsString[i] = itemsString[i].split("").reverse().join("");
+      k++;
+      //console.log(itemsString[i]);
+    }
     
-  }
+    let lastNumber ='' + itemsString[itemsString.length - 1];
+    //lastNumber самое big number почся
+    //lastNumber 
+    let test =  itemsString[itemsString.length - 2];
+    for( let i = 0; i < lastNumber.length; i++ ){
+      for( let ii = 0 ; ii <itemsString.length; ii++){
+        if(!itemsString[ii][i]){
+            itemsString[ii] = itemsString[ii]  + '0';
+        }
+      }
+    }
+    for(let i = 0; i < itemsString.length ; i++ ){
+      console.log(itemsString[i]);
+    }
+    
+    let len = itemsString[0].length;
+    var finalNumber = [];
+    let resultColumn;
+    let add = 0;
+    for(let i = 0; i < len; i++){
+      resultColumn = 0;
+      for(let ii = 0; ii < itemsString.length; ii++){
+        
+        resultColumn += parseInt(itemsString[ii][i]) ;
+      }
+      resultColumn += add;
+         if(resultColumn > 9){
+          finalNumber.push(resultColumn%10);
+          add = parseInt(resultColumn/10);
+        }else{
+          finalNumber.push(resultColumn);
+          add = 0;
+        }
+      //console.log(resultColumn);
+    }
+   
+    if(!finalNumber[finalNumber.length -1 ]){
+      finalNumber[finalNumber.length -1 ] ='';
+    }
 
-  return itemString;
+ 
+
+
+
+    
+    
+    return finalNumber.reverse().join("");
 }
 
